5912b9b366d67d937240e239cc2df8f7ba5912da
Vanja Šerfeze <serfezev@gmail.com>
2022-11-07 21:57:32 +0100

    dodat dto i pribavljanje profila

---
+++ uber-clone/src/main/java/com/backend/uberclone/service/ProfileService.java
@@ -0,0 +1,21 @@
+package com.backend.uberclone.service;
+
+import com.backend.uberclone.dto.UserDTO;
+import com.backend.uberclone.model.User;
+import com.backend.uberclone.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfileService {
+    private UserRepository<User> userRepository;
+
+    @Autowired
+    private void setUserRepository(UserRepository<User> userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDTO getProfileInfo(String email) {
+        return new UserDTO(userRepository.findUserByEmailAndBaAndBannedFalseAndEnabledTrueAndAndDeletedFalse(email));
+    }
+}
