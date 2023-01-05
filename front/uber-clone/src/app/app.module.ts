8af9a951ed720ea3dd70d3932937312882302b90
aleksa-hubgit <aleksa.stev92@gmail.com>
2023-01-05 04:35:25 +0100

    Dodat inicijalizovan angular projekat sa pocetnom stranicom.

---
+++ front/uber-clone/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormMapContainerComponent } from './form-map-container/form-map-container.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormMapContainerComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ front/uber-clone/src/app/form-map-container/form-map-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-map-container',
+  templateUrl: './form-map-container.component.html',
+  styleUrls: ['./form-map-container.component.scss']
+})
+export class FormMapContainerComponent {
+
+}
