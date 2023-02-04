6765ed5d24e532a0807b9610975f8cc675f2d6d2
unknown <stefan.milosevic.e14@gmail.com>
2023-02-04 05:57:59 +0100

    Podesena baza za testove

--- uber-clone/src/test/java/com/backend/uberclone/repository/RideRepositoryTests.java
+++ uber-clone/src/test/java/com/backend/uberclone/repository/RideRepositoryTests.java
@@ -2,19 +2,26 @@ package com.backend.uberclone.repository;
 
 import com.backend.uberclone.model.Ride;
 import com.backend.uberclone.model.RideStatus;
+import com.backend.uberclone.model.User;
 import com.backend.uberclone.service.RideService;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 public class RideRepositoryTests {
-    private RideService rideRepository;
+    @Autowired
+    private UserRepository<User> userRepository;
 
    @Test
     public void findByIdAndStatusAndDriverId(){
-       rideRepository.refundClients(new Ride());
+       User u = userRepository.findOneById(1);
+       assertEquals("bubibubisa@gmail.com",u.getEmail());
 
    }
 }
